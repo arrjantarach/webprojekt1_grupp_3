4d0eba6e8af079db5efe175c34c71664e000726d
Therese <therese.kr@hotmail.com>
2026-09-22 10:11:08 +0200

    lagt till footer.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-fetch('navbar.html').
-then(response => response.text()).
-then(data => {document.getElementById('navbar-placeholder').innerHTML = data;});
+fetch("navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
+  });
+
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+    const btn = document
+      .getElementById("toTopBtn")
+      .addEventListener("click", () =>
+        window.scrollTo({ top: 0, behavior: "smooth" }),
+      );
+  });
